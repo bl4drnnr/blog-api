c704be64eaf700e06829b8befaa2569425275441
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-08-13 10:03:51 +0200

    finally cookie

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as cookieParser from 'cookie-parser';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
@@ -9,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api/');
-  app.use(cookieParser());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe());
 
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  UseGuards,
-  Response
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { SignUpUserDto } from '../dto/user/sign-up-user.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -18,8 +11,6 @@ import { BanUserDto } from '../dto/user/ban-user.dto';
 import { UserBan } from '../models/user-ban.model';
 import { User as UserDecorator } from '../decorator/user.decorator';
 import { TokensDto } from '../dto/token/tokens.dto';
-import { Response as Res } from 'express';
-import * as dayjs from 'dayjs';
 
 @ApiTags('User')
 @Controller('user')
@@ -29,15 +20,8 @@ export class UserController {
   @ApiOperation({ summary: 'Resource for sign in user.' })
   @ApiResponse({ status: 200, type: TokensDto })
   @Post('/sign-in')
-  async signIn(@Body() signInUserDto: SignInUserDto, @Response() res: Res) {
-    const { _at, _rt } = await this.userService.signIn(signInUserDto);
-
-    res.cookie('_rt', _rt, {
-      httpOnly: true,
-      expires: dayjs().add(7, 'days').toDate()
-    });
-
-    return res.send(_at);
+  async signIn(@Body() signInUserDto: SignInUserDto) {
+    return this.userService.signIn(signInUserDto);
   }
 
   @ApiOperation({ summary: 'Resource for sign up user.' })
