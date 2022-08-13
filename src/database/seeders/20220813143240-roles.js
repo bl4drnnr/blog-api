9a57d75c58deb91308a5456900c54253fcb966ab
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-08-13 17:05:18 +0200

    seeder fixes

--- src/database/seeders/20220813143240-roles.js
+++ src/database/seeders/20220813143240-roles.js
@@ -6,12 +6,18 @@ module.exports = {
       'Roles',
       [
         {
+          id: 'c77a2e92-fef0-47ba-8d8c-14074e0c8971',
           value: 'USER',
-          description: 'Common user'
+          description: 'Common user',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
+          id: 'b622c0ee-0ac8-4486-99e2-a9e2384e4c0e',
           value: 'ADMIN',
-          description: 'Administrator'
+          description: 'Administrator',
+          createdAt: new Date(),
+          updatedAt: new Date()
         }
       ],
       {}
--- src/database/seeders/20220813143249-posts.js
+++ src/database/seeders/20220813143249-posts.js
@@ -6,19 +6,28 @@ module.exports = {
       'Posts',
       [
         {
+          id: '5d79b8b1-f677-47af-8bcb-958987711965',
           title: 'How JS is running the world',
           slug: 'how-js-is-running-the-world',
-          content: 'Here is some cool content'
+          content: 'Here is some cool content about JS',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
+          id: 'c33530a1-6318-40fb-ab20-4f7adec6d02a',
           title: 'Python and everything you can do with this',
           slug: 'python-and-everything-you-can-do-with-this',
-          content: 'Here is some cool content'
+          content: 'Here is some cool content about Python',
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
+          id: '217c2312-0e39-4be0-b298-386caf16906d',
           title: 'C++ - pros and cons',
           slug: 'c++-pros-and-cons',
-          content: 'Here is some cool content'
+          content: 'Here is some cool content about C++',
+          createdAt: new Date(),
+          updatedAt: new Date()
         }
       ],
       {}
